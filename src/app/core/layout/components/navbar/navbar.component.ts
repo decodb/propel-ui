95d5ef8d8612bc7f9aba05ab8d2746bb6dd62149
Buti Donovan Mkhwanazi <mkwanazibuti082@gmail.com>
2026-04-15 23:50:45 +0200

    Public Pages (home)

--- src/app/core/layout/components/navbar/navbar.component.ts
+++ src/app/core/layout/components/navbar/navbar.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, HostListener, signal } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  isScrolled = signal<boolean>(false)
 
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    this.isScrolled.set(window.scrollY > 50); 
+  }
 }
--- src/app/core/layout/public/pages/home/home.component.ts
+++ src/app/core/layout/public/pages/home/home.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { NavbarComponent } from '../../../components/navbar/navbar.component';
+import { RouterLink } from "@angular/router";
+import { FooterComponent } from "../../../components/footer/footer.component";
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, RouterLink, FooterComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
