8e13138cacfc9dd5aba6a5c86a5659b3832bb46e
Buti Donovan Mkhwanazi <mkwanazibuti082@gmail.com>
2026-04-16 20:26:51 +0200

    Code Rabbit reviews / changes

--- src/app/core/layout/components/navbar/navbar.component.ts
+++ src/app/core/layout/components/navbar/navbar.component.ts
@@ -1,17 +1,22 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, HostListener, signal } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, HostListener, inject, PLATFORM_ID,signal } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
   isScrolled = signal<boolean>(false)
+  private platformId = inject(PLATFORM_ID);
 
   @HostListener('window:scroll', [])
   onScroll(): void {
-    this.isScrolled.set(window.scrollY > 50); 
+    if (isPlatformBrowser(this.platformId)) {
+      this.isScrolled.set(window.scrollY > 50);
+    }
   }
 }
