4228c9483838ff39244b1170efb30666958d163b
Buti Donovan Mkhwanazi <mkwanazibuti082@gmail.com>
2026-04-18 22:29:03 +0200

    Sign up UI

--- src/app/core/layout/public/pages/sign-up/sign-up.component.ts
+++ src/app/core/layout/public/pages/sign-up/sign-up.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, EventEmitter, Output, signal } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [RouterLink],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss'
 })
 export class SignUpComponent {
+  @Output() fileSelected = new EventEmitter<File>();
+  showPassword = signal<boolean>(false);
 
+  selectedFile: File | null = null;
+  isDragging = false;
+
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files?.length) this.setFile(input.files[0]);
+  }
+
+  onDragOver(event: DragEvent): void {
+    event.preventDefault();
+    this.isDragging = true;
+  }
+
+  onDrop(event: DragEvent): void {
+    event.preventDefault();
+    this.isDragging = false;
+    const file = event.dataTransfer?.files[0];
+    if (file) this.setFile(file);
+  }
+
+  removeFile(event: MouseEvent): void {
+    event.stopPropagation();
+    this.selectedFile = null;
+  }
+
+  private setFile(file: File): void {
+    this.selectedFile = file;
+    this.fileSelected.emit(file);
+  }
+
+  togglePassword() {
+    this.showPassword.set(!this.showPassword())
+  }
 }
