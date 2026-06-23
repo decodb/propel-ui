47b5b0b4ee2a345998199cad84a91fecd526d3d3
Buti Donovan Mkhwanazi <mkwanazibuti082@gmail.com>
2026-06-23 17:20:47 +0200

    feat(admin): implement company management dashboard UI

--- src/app/features/system-admin/dashboard/dashboard.component.ts
+++ src/app/features/system-admin/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
-
+  page: Number = 1;
 }
